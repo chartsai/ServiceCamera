7de529b5c8e9dc0fce43fec4b09d640dcb5cd618
Ymow Wu <kenshin0812@hotmail.com>
2016-03-27 15:53:35 +0800

    make video preview after recording
    delete useless code

--- app/src/main/java/example/chatea/servicecamera/MainActivity.java
+++ app/src/main/java/example/chatea/servicecamera/MainActivity.java
@@ -2,25 +2,52 @@ package example.chatea.servicecamera;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.session.MediaController;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
 
-    private Button bt_recordingButton;
+    private Button bt_recordingButton, bt_playingVideoButton;
+    private VideoView mVideoView;
+    private String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mVideoView = (VideoView) findViewById(R.id.videoview);
+        bt_playingVideoButton = (Button) findViewById(R.id.play_video_button);
         bt_recordingButton = (Button) findViewById(R.id.recording_button);
         bt_recordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startRecording();
+            }
+        });
+
+        bt_playingVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String DataPath = CameraService.DataPath;
+                Log.d(TAG, DataPath);
+
+                mVideoView.setVideoPath(DataPath);
+//                mVideoView.setMediaController(new MediaController(this)); //API 21
+                mVideoView.requestFocus();
+                mVideoView.start();
             }
         });
     }
@@ -29,4 +56,7 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, CameraService.class);
         startService(intent);
     }
+
+
+
 }
